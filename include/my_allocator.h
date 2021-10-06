67a4aed7af9bc552bb248c5299538ea25d0431a1
r.serra <r.serra@student.isartdigital.com>
2021-10-06 18:32:31 +0200

    Added work on the malloc project (done malloc, free and realloc)

---
+++ include/my_allocator.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <stddef.h>
+ 
+void* my_alloc(size_t size);
+void* my_realloc(void* ptr, size_t size);
+void* my_calloc(size_t nb, size_t size);
+void my_free(void* ptr);
+void showDebugInfo(); // Display information on metadata.
