90e0aa0e04ab90911b340796b351879d17c72238
r.serra <r.serra@student.isartdigital.com>
2021-10-08 16:02:54 +0200

    All functions are now working. Added the malloc hook function so that my malloc functions are used when malloc() is called.

--- include/my_allocator.h
+++ include/my_allocator.h
@@ -1,8 +1,16 @@
 #pragma once
+#include <malloc.h>
+
+void registerAllocatorHook(void);
+void showDebugInfo(void); // Display information on metadata
+
+/*
 #include <stddef.h>
- 
-void* my_alloc(size_t size);
+
+void* my_malloc(size_t size);
 void* my_realloc(void* ptr, size_t size);
 void* my_calloc(size_t nb, size_t size);
 void my_free(void* ptr);
 void showDebugInfo(); // Display information on metadata.
+*/
+
--- tests/test_allocator.c
+++ tests/test_allocator.c
@@ -1,50 +1,55 @@
 #include <stdlib.h>
 #include <stdio.h>
+ 
 #include <my_allocator.h>
- 
-#define USE_MY_ALLOCATOR
- 
-#ifdef USE_MY_ALLOCATOR
-#define ALLOC my_alloc
-#define REALLOC my_realloc
-#define CALLOC my_calloc
-#define FREE my_free
-#else
-#define ALLOC malloc
-#define REALLOC realloc
-#define CALLOC calloc
-#define FREE free
-#endif
+
  
 void test1()
 {
    // MALLOC
-   int* a = ALLOC(sizeof(int));
+   int* a = malloc(sizeof(int));
    *a = 10;
    printf("(malloc) a = %d\n", *a);
  
    // REALLOC
-   a = REALLOC(a, 2 * sizeof(int));
+   a = realloc(a, 2 * sizeof(int));
    a[1] = 20;
    printf("(realloc) a[1] = %d\n", a[1]);
 
    // FREE
-   FREE(a); 
+   free(a); 
  
    // CALLOC
-   a = CALLOC(1, sizeof(int));
+   a = calloc(1, sizeof(int));
    printf("(calloc) a = %d\n", *a);
-   FREE(a);
+   free(a);
 }
+
+
+void testAlloc()
+{
+   // MALLOC
+   int* a = malloc(sizeof(int));
+   *a = 10;
+   printf("(malloc) a = %d\n", *a);
+
+   showDebugInfo();
+}
+
+
 
 void my_test()
 {
-    printf("\n");
+    showDebugInfo();
+
+    // Callocate an int array.
+    int* val0 = calloc(49, sizeof(int));
+    printf("Callocated int[49] val8, first 3 values: %d, %d, %d.\n", val0[0], val0[1], val0[2]);
 
     showDebugInfo();
 
     // Allocate a string.
-    char* val1 = my_alloc(sizeof(char) * 13);
+    char* val1 = malloc(sizeof(char) * 14);
     val1[0] = 'H';
     val1[1] = 'e';
     val1[2] = 'l';
@@ -63,80 +68,67 @@ void my_test()
 
     showDebugInfo();
 
-    // Callocate an int array.
-    int* val8 = my_calloc(49, sizeof(int));
-    printf("Callocated int[49] val8, first 3 values: %d, %d, %d.\n", val8[0], val8[1], val8[2]);
-
-    showDebugInfo();
-
     // Allocate an int.
-    int* val2 = my_alloc(sizeof(int));
+    int* val2 = malloc(sizeof(int));
     *val2 = 5;
     printf("Allocated int val2 of value: %d.\n", *val2);
 
     showDebugInfo();
 
     // Free val8.
-    my_free(val8);
+    free(val0);
     printf("Freed int[50] val8 of first 3 values: 0, 0, 0.\n");
 
     showDebugInfo();
 
-    // Free val2.
-    my_free(val2);
-    printf("Freed int val2 of value: 5.\n");
-
-    showDebugInfo();
-
     // Reallocate val3.
-    int* val3 = my_alloc(sizeof(int));
-    *val3 = 10;
-    printf("Allocated int val3 of value: %d.\n", *val3);
+    int* val3 = malloc(sizeof(int) * 5);
+    val3[0] = 10;
+    val3[1] = 5;
+    printf("Allocated int[2] val3 of first 2 values: %d.\n", val3[0], val3[1]);
 
     showDebugInfo();
 
     // Free val1.
-    my_free(val1);
+    free(val1);
     printf("Freed char val1 of value: Hello, World!.\n");
 
     showDebugInfo();
 
     // Allocate another int.
-    int* val4 = my_alloc(sizeof(int));
-    *val4 = 15;
+    int* val4 = malloc(sizeof(int));
+    *val4 = 97;
     printf("Allocated int val4 of value: %d.\n", *val4);
 
     showDebugInfo();
 
-    // Reallocate the val3.
-    int* val5 = my_realloc(val3, sizeof(int));
-    *val5 = 20;
+    // Reallocate val3.
+    int* val5 = realloc(val3, sizeof(int));
     printf("Reallocated int val3 to variable val5 of value: %d.\n", *val5);
 
     showDebugInfo();
 
     // Reallocate val4.
-    char* val6 = my_realloc(val4, sizeof(char));
-    *val6 = 'a';
+    char* val6 = realloc(val4, sizeof(char));
     printf("Reallocated int val4 to variable char val6 of value: %c.\n", *val6);
 
     showDebugInfo();
 
     // Reallocate val5.
-    long long int* val7 = my_realloc(val5, sizeof(long long int));
+    long long int* val7 = realloc(val5, sizeof(long long int));
     *val7 = 1;
     printf("Reallocated int val5 to variable val7 of value: %lld.\n", *val7);
 
     showDebugInfo();
 
     // Free val7 using realloc.
-    my_realloc(val7, 0);
+    realloc(val7, 0);
     printf("Using realloc, freed long long int val7 of value: 1.\n");
 
     showDebugInfo();
 
     // Free val 6.
-    my_free(val6);
+    free(val6);
     printf("Freed char val6 of value: a.\n");
 
     showDebugInfo();
@@ -145,11 +137,9 @@ void my_test()
  
 int main()
 {
-    #ifdef USE_MY_ALLOCATOR
+    registerAllocatorHook();
+
     my_test();
-    #else
-    test1();
-    #endif
 
     return 0;
 }
