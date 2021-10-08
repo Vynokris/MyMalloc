2c52ab9213cfd145d0c00d1cc070dac5cddc2d10
Vynokris <vynokris@gmail.com>
2021-10-08 09:58:07 +0200

    Modified the MetaData struct to make it more space efficient (from 32 bytes to 16). This also required a rework of the my_allocator.c code.

--- src/metadata.h
+++ src/metadata.h
@@ -6,12 +6,19 @@
 typedef struct MetaData MetaData;
 
 typedef struct MetaData {
-    void* data;
     bool free;
-    size_t size;
     MetaData* next;
 } MetaData;
 
 
 // Returns the meta-data of all allocated memory.
 static MetaData** get_metadata();
+
+// Returns the data of a metadata block.
+static void* get_data(MetaData* metadata_block);
+
+// Returns the size of the data storded after a metadata block.
+static int get_data_size(MetaData* metadata_block);
+
+// Returns the size of a metadata block.
+static int get_metadata_size(MetaData* metadata_block);
--- src/my_allocator.c
+++ src/my_allocator.c
@@ -2,14 +2,33 @@
 #include "metadata.h"
 
 
-// TODO: Figure out how to get calloc to work.
-// TODO: Merge two free memory blocks if they are consecutive.
-
-
 static MetaData** get_metadata()
 {
     static MetaData* metadata = NULL;
     return &metadata;
+}
+
+
+static void* get_data(MetaData* metadata_block)
+{
+    return (void*)metadata_block + sizeof(MetaData);
+}
+
+
+static int get_data_size(MetaData* metadata_block)
+{
+    if (metadata_block->next != NULL) {
+        return (void*)metadata_block->next - get_data(metadata_block);
+    }
+    else {
+        return sbrk(0) - (int)get_data(metadata_block);
+    }
+}
+
+
+static int get_metadata_size(MetaData* metadata_block)
+{
+    return get_data(metadata_block) - (void*)metadata_block;
 }
 
 
@@ -19,7 +38,7 @@ void showDebugInfo()
     MetaData* metadata = *get_metadata();
 
     if (metadata == NULL) {
-        printf("\nNo data\n");
+        printf("\nNo data.\n");
     }
     else {
         printf("\nAllocated data:\n");
@@ -27,16 +46,14 @@ void showDebugInfo()
         // Loop through the elements and print them.
         MetaData* current = metadata;
         for (int i = 0; current != NULL; i++) {
-            if (current->next != NULL)
-                printf("%d:\n metadata adress = %p\n metadata size = %d\n data adress = %p\n data size = %d\n free = %s\n", i, current, current->data - (void*)current, current->data, (void*)current->next - current->data, (current->free ? "true":"false"));
-            else
-                printf("%d:\n metadata adress = %p\n metadata size = %d\n data adress = %p\n data size = %d\n free = %s\n", i, current, current->data - (void*)current, current->data, sbrk(0) - (int)current->data, (current->free ? "true":"false"));
-
+            printf("%d:\n metadata adress = %p\n metadata size = %d\n data adress = %p\n data size = %d\n free = %s\n", 
+                    i, current, get_metadata_size(current), get_data(current), get_data_size(current), (current->free ? "true":"false"));
+            
             current = current->next;
         }
     }
 
-    printf("Break adress = %p\n\n", sbrk(0));
+    printf("Break adress: %p\n\n", sbrk(0));
 }
 
 
@@ -49,50 +66,49 @@ void* my_alloc(size_t size)
     if (*metadata == NULL) 
     {
         *metadata = sbrk(sizeof(MetaData));
-        **metadata = (MetaData){ sbrk(size), false, size, NULL };
-        return (*metadata)->data;
+        sbrk(size);
+        **metadata = (MetaData){ false, NULL };
+        return get_data(*metadata);
     }
 
     else 
     {
         // Find a free memory block in the heap.
         MetaData* mem_block = *metadata;
-        while (!(mem_block->free && mem_block->size >= sizeof(MetaData) + size) && mem_block->next != NULL) {
+        while (!(mem_block->free && get_data_size(mem_block) >= sizeof(MetaData) + size) && mem_block->next != NULL) {
             mem_block = mem_block->next;
         }
 
         // If the found memory block is after the break, move the break and return.
         if (mem_block->next == NULL) {
             mem_block->next = sbrk(sizeof(MetaData));
-            *(mem_block->next) = (MetaData){ sbrk(size), false, size, NULL };
-            return mem_block->next->data;
+            sbrk(size);
+            *(mem_block->next) = (MetaData){ false, NULL };
+            return get_data(mem_block->next);
         }
 
         // If the found memory is freed memory...
         else {
             // If the found memory is just the right size, set it to used and return.
-            if (mem_block->size == size) {
+            if (get_data_size(mem_block) == size) {
                 mem_block->free = false;
-                return mem_block->data;
+                return get_data(mem_block);
             }
 
             // If the found memory is too large, set the necessary size to used, leave the remaining free and return.
             else {
                 // Get the values for the meta-data of the new memory block.
-                void* new_md_adress = mem_block->data + size;
-                size_t new_size = mem_block->size - sizeof(MetaData) - size;
+                void* new_md_adress = get_data(mem_block) + size;
                 MetaData* next_metadata = mem_block->next;
 
                 // Reallocate the found memory.
                 mem_block->free = false;
-                mem_block->size = size;
 
                 // Set the meta-data values for the new memory block.
                 mem_block->next = new_md_adress;
-                *(mem_block->next) = (MetaData){ new_md_adress + sizeof(MetaData), new_size, true, next_metadata };
-                mem_block->next->size = new_size; // For some reason this is necessary: even though the new size is already set on the previous line, it isn't set to the right value.
+                *(mem_block->next) = (MetaData){ true, next_metadata };
 
-                return mem_block->data;
+                return get_data(mem_block);
             }
         }
     }
@@ -130,12 +146,12 @@ void my_free(void* ptr)
     // Meta-data of all allocated memory.
     MetaData** metadata = get_metadata();
 
-    // If the metadata list is empty, return NULL.
+    // If the metadata list is not empty...
     if (*metadata != NULL) 
     {
         // Find the memory block to be freed.
         MetaData* mem_block = *metadata;
-        while (mem_block && mem_block->data != ptr) {
+        while (mem_block && get_data(mem_block) != ptr) {
             mem_block = mem_block->next;
         }
 
@@ -143,39 +159,38 @@ void my_free(void* ptr)
         if (mem_block != NULL) {
             mem_block->free = true;
         }
-    }
 
-    // Check for consecutive free memory blocks and merge them.
-    MetaData* cur_mem_block = *metadata;
-    MetaData* last_mem_block = *metadata;
+        // Check for consecutive free memory blocks and merge them.
+        MetaData* cur_mem_block = *metadata;
+        MetaData* last_mem_block = *metadata;
 
-    while (cur_mem_block->next != NULL) {
-        cur_mem_block = cur_mem_block->next;
+        while (cur_mem_block->next != NULL) {
+            cur_mem_block = cur_mem_block->next;
 
-        // If the current and the precedent memory block are free...
-        if (cur_mem_block->free && last_mem_block->free) {
-            last_mem_block->size += sizeof(MetaData) + cur_mem_block->size;
-            last_mem_block->next = cur_mem_block->next;
-            cur_mem_block = last_mem_block;
+            // If the current and the precedent memory block are free, merge them
+            if (cur_mem_block->free && last_mem_block->free) {
+                last_mem_block->next = cur_mem_block->next;
+                cur_mem_block = last_mem_block;
+            }
+            else {
+                last_mem_block = cur_mem_block;
+            }
+        }
+
+        // Check if the last memory block is free and move the break back if so.
+        if ((*metadata)->next == NULL) {
+            brk(cur_mem_block);
+            *metadata = NULL;
         }
         else {
-            last_mem_block = cur_mem_block;
-        }
-    }
-
-    // Check if the last memory block is free and move the break back if so.
-    if ((*metadata)->next == NULL) {
-        brk(cur_mem_block);
-        *metadata = NULL;
-    }
-    else {
-        cur_mem_block = *metadata;
-        while (cur_mem_block->next->next != NULL) {
-            cur_mem_block = cur_mem_block->next;
-        }
-        if (cur_mem_block->next->free) {
-            brk(cur_mem_block->next);
-            cur_mem_block->next = NULL;
+            cur_mem_block = *metadata;
+            while (cur_mem_block->next->next != NULL) {
+                cur_mem_block = cur_mem_block->next;
+            }
+            if (cur_mem_block->next->free) {
+                brk(cur_mem_block->next);
+                cur_mem_block->next = NULL;
+            }
         }
     }
 }
