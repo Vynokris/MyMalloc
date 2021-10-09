f4e4ee9be4ecffe0509477349213017e488c971d
Vynokris <vynokris@gmail.com>
2021-10-09 22:22:36 +0200

    Finished polishing the memory allocation and free functions. The only thing left to do is to make sure with Paul that my realloc is legit.

--- src/metadata.h
+++ src/metadata.h
@@ -23,6 +23,9 @@ static size_t get_data_size(MetaData* metadata_block);
 // Returns the size of a metadata block.
 static size_t get_metadata_size(MetaData* metadata_block);
 
+// Returns true if the given metadata block is the last one of the list and false if not.
+static bool is_end_of_list(MetaData* metadata_block);
+
 // Returns the smallest number superior to size that is divisible ny 8.
 static size_t get_aligned_size(size_t size);
 
@@ -31,3 +34,7 @@ static void* alloc_metadata_and_data(MetaData** metadata_block, size_t size);
 
 // Splits a free memory block in two so that half of it can be used.
 static void* split_mem_block(MetaData* mem_block, size_t size);
+
+// Attempts to merge the given memory block with the following one and the one after it.
+// The given block should be the memory block before a memory block to be freed.
+static void* merge_mem_block(MetaData* mem_block);
